7cb80504974a258bcec2752755fe081d36932182
nick <nick@ogc.us>
2022-11-14 12:36:25 -0600

    Remove node_modules

--- node_modules/@grafana/ui/components/Segment/useExpandableLabel.d.ts
+++
@@ -1,8 +0,0 @@
-import React, { ReactElement } from 'react';
-interface LabelProps {
-    Component: ReactElement;
-    onClick?: () => void;
-    disabled?: boolean;
-}
-export declare const useExpandableLabel: (initialExpanded: boolean) => [React.ComponentType<LabelProps>, number, boolean, (expanded: boolean) => void];
-export {};
